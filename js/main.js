197a0000409031db4b7a8cf440252ca28158e292
Alzagab <98777206+Alzagab@users.noreply.github.com>
2022-02-04 01:39:36 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const elementosCarousel = document.querySelectorAll('.carousel');
+	M.Carousel.init(elementosCarousel, {
+		duration: 400,
+		dist:5
+        
+	});
+});
